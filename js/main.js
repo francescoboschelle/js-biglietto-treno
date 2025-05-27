509dfac6f7673fb772552035c82358a09d5d1880
francescoboschelle <francibossch@gmail.com>
2025-05-27 14:38:01 +0200

    Refactor input validation for distance and age in ticket price calculation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 const distance = Number(prompt("Quanti chilometri vuoi percorrere?"));
 
-if (!distance || isNaN(distance)) {
+if (distance && !isNaN(distance)) {
+  const age = Number(prompt("Che etá hai?"));
+
+  if (age && !isNaN(age)) {
+    let cost = Math.floor(0.21 * distance);
+
+    if (age < 18) {
+      cost = cost * 0.8;
+    } else if (age > 65) {
+      cost = cost * 0.6;
+    }
+
+    alert(`Il biglietto costerá approssimativamente: ${cost.toFixed(2)} `);
+  } else {
+    alert("Devi selezionare un numero valido!");
+  }
+} else {
   alert("Devi selezionare un numero valido!");
 }
-
-const age = Number(prompt("Che etá hai?"));
-
-if (!age || isNaN(age)) {
-  alert("Devi selezionare un numero valido!");
-}
-
-let cost = Math.floor(0.21 * distance);
-
-if (age < 18) {
-  cost = cost * 0.8;
-} else if (age > 65) {
-  cost = cost * 0.6;
-}
-
-alert(`Il biglietto costerá approssimativamente: ${cost.toFixed(2)} `);
